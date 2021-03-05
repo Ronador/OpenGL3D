fa7b13ee3b6c17d8158e2d83528f989fac8dcdc8
Gaiuss <helloitsyang@gmail.com>
2021-03-05 14:57:22 +0800

    模型

--- app/src/main/java/com/gaius/opgl3d/MainActivity.java
+++ app/src/main/java/com/gaius/opgl3d/MainActivity.java
@@ -1,14 +1,36 @@
 package com.gaius.opgl3d;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        glSurfaceView = findViewById(R.id.gl_sf_view);
+        glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        glSurfaceView.setRenderer(new EgGLRenderer(getApplicationContext()));
+        glSurfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        glSurfaceView.setZOrderOnTop(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
     }
 }
